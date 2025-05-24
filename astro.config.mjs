d66a60d663ec2ebe8d09af9c76d8533879d80ea8
O-Isaac <soporte@proyectograndorder.es>
2025-05-24 13:36:11 +0200

    feat: Update Astro configuration to use Vercel adapter and standardize import formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 import clerk from "@clerk/astro";
-import { esES } from "@clerk/localizations"
+import { esES } from "@clerk/localizations";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk({ localization: esES }), db(), react()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
   server: {
-    allowedHosts: [
-      ".trycloudflare.com"
-    ]
+    allowedHosts: [".trycloudflare.com"],
   },
   output: "server",
 });
