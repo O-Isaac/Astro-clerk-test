84a178699de7454170660a9221b24173e0506696
O-Isaac <soporte@proyectograndorder.es>
2025-05-24 16:15:54 +0200

    feat: Add branding API route for redirection to corebook

---
+++ src/pages/branding.ts
@@ -0,0 +1,5 @@
+import type { APIRoute } from "astro";
+
+export const ALL: APIRoute = async ({ request, locals, redirect }) => {
+    return redirect("https://my.corebook.io/aOgFm6dArNa2GrcWTv4x90RlosJ6LnBI");
+}
